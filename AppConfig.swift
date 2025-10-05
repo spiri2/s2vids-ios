a0b875d9e7a7a624885fd50587b8a3b5d57949aa
Spiri2 <60682584+spiri2@users.noreply.github.com>
2025-10-05 15:22:54 -0700

    Update AppConfig.swift

--- AppConfig.swift
+++ AppConfig.swift
@@ -10,7 +10,7 @@ enum AppConfig {
   static let supabaseURL = URL(string: "https://zguwfdtrmbabxfpexurt.supabase.co")!
 
   // Replace with your *full* Anon public key — no spaces, no line breaks, no “>”
-  static let supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpndXdmZHRybWJhYnhmcGV4dXJ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkzNTcxMjgsImV4cCI6MjA2NDkzMzEyOH0.8V7"
+  static let supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpndXdmZHRybWJhYnhmcGV4dXJ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkzNTcxMjgsImV4cCI6MjA2NDkzMzEyOH0.8V7TNBfhT92W-gmYMhU1lG8hF_UiffOqUpszfJAxjSo"
 
   // API base for your custom Next.js endpoints
   static let apiBase = URL(string: "https://s2vids.org")!
