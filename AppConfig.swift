a62e4fa9153351fe13d2d91e480a5f686a2a1e51
Spiri2 <60682584+spiri2@users.noreply.github.com>
2025-10-05 14:45:21 -0700

    Update AppConfig.swift

--- AppConfig.swift
+++ AppConfig.swift
@@ -8,7 +8,7 @@ import Foundation
 enum AppConfig {
   // Supabase configuration
   static let supabaseURL = URL(string: "https://zguwfdtrmbabxfpexurt.supabase.co")!
-  static let supabaseAnonKey = " eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpndXdmZHRybWJhYnhmcGV4dXJ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkzNTcxMjgsImV4cCI6MjA2NDkzMzEyOH0.8V7>"
+  static let supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpndXdmZHRybWJhYnhmcGV4dXJ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkzNTcxMjgsImV4cCI6MjA2NDkzMzEyOH0.8V7>"
 
   // API base for custom routes
   static let apiBase = URL(string: "https://s2vids.org")!
